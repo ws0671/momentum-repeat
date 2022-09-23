95f149ef2664cdcb2380f0beba383d7384f37e4d
ws0671 <ws0671@naver.com>
2022-09-23 21:12:56 +0900

    Feat: 로그인 기능

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ const loginForm = document.getElementById("login-form");
 const loginInput = document.querySelector("#login-form > input");
 const title = document.getElementById("title");
 
+// 같은 문자열을 두 번 이상 사용할 경우 변수로 만들어주면 좋음.
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
 function handleLoginForm(e) {
   e.preventDefault();
   const username = loginInput.value;
-  loginForm.classList.add("hidden");
-  title.innerText = `Welcome ${username}`;
-  title.classList.remove("hidden");
+  localStorage.setItem(USERNAME_KEY, username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintingTitle(username);
 }
 
-loginForm.addEventListener("submit", handleLoginForm);
+function paintingTitle(username) {
+  title.innerText = `Welcome ${username}`;
+  title.classList.remove(HIDDEN_CLASSNAME);
+}
+
+let savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", handleLoginForm);
+} else {
+  paintingTitle(savedUserName);
+}
